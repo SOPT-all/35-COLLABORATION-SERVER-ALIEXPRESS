415b252459f6d5ba49a1b53196ffed1367905c17
Yunhyuk Jeon <137145628+airoca@users.noreply.github.com>
2024-11-19 20:00:46 +0900

    [feat] Initialize project

---
+++ src/main/java/com/example/aliexpress/AliexpressApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aliexpress/AliexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aliexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
